3d5be44efde2868420a47d0cd916423a13c60d51
Nick Trotsyuk <44951382+RobotToI@users.noreply.github.com>
2021-02-10 08:29:50 +0300

    Update game_stats.py
    Just edit tabs

--- game_stats.py
+++ game_stats.py
@@ -9,21 +9,23 @@ class GameStats():
         # Game starts with non-active mode
         self.game_status = False
 
+        
     def reset_status(self):
         '''Initialize statistics, that was changed by game procces'''
         self.ships_left = self.ai_settings.ship_limit
         self.score = 0
         self.level = 1
 
+        
     def get_high_score_from_file(self):
         '''We just return highest score frome file'''
-            try:
-                hs = open('record', 'r')
-                self.high_score = int(hs.readline())
-                hs.close()
-            except:
-                hs = open('record', 'r')
-                self.high_score = 0
-                hs.write('0')
-                hs.close()
+        try:
+            hs = open('record', 'r')
+            self.high_score = int(hs.readline())
+            hs.close()
+        except:
+            hs = open('record', 'r')
+            self.high_score = 0
+            hs.write('0')
+            hs.close()
 
